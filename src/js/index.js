585080a8da128c2f0a788867a43f2a8db91050ab
Jonathan Henrique <jonathanbispo085@gmail.com>
2022-10-09 12:42:30 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 const advanceClick = document.getElementById("advance");
 const returnClick = document.getElementById("return");
 const cards = document.querySelectorAll(".card");
+
+
 let actualCard = 0;
 
 function hideSelectedCard() {
@@ -41,3 +43,25 @@ returnClick.addEventListener("click", function () {
   actualCard--;
   addSelectedClass();
 });
+
+function audioPlay(){
+  if(actualCard === 0){
+    const akali = document.getElementById('akali-voice');
+    akali.play();
+  }else if (actualCard === 1){
+    const yasuo = document.getElementById('yasuo-voice');
+    yasuo.play();
+  }else if(actualCard === 2){
+    const irelia = document.getElementById('irelia-voice');
+    irelia.play();
+  }else if(actualCard === 3){
+    const draven = document.getElementById('draven-voice');
+    draven.play();
+  }else if(actualCard === 4){
+    const riven = document.getElementById('riven-voice');
+    riven.play();
+  }else if(actualCard === 5){
+    const yone = document.getElementById('yone-voice');
+    yone.play();
+  }
+}
